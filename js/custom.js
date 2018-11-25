74bef788b78b7674edbf898abe41c1ab81c1d03b
Akshay Blog WebApp <akshayboddhul45@gmail.com>
2018-11-25 20:00:39 +0530

    Added Brands

--- js/custom.js
+++ js/custom.js
@@ -123,3 +123,29 @@ $(function(){
         }, 1250);
     });
 });
+
+//Brand
+$(function () {
+
+    $("#brand-list").owlCarousel({
+        items: 6,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+		responsive: {
+		  0: {
+			items: 1
+		  },
+		  480: {
+			items: 3
+		  },
+		  768: {
+			items: 5
+		  },
+		  992: {
+			items: 6
+		  }
+		}
+    });
+});
